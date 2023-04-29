59db485a5ee8d5b329c1826cc14e81482b05170c
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-04-28 22:43:35 -0300

    commit the exercise python61.py and change python50.py and 60

--- python61.py
+++ python61.py
@@ -1,14 +1,19 @@
-print('\033[1mENÉSIMO PRIMEIROS TERMOS DE UMA PA (PROGRESSÃO ARITMÉTICA)\033[m')
+print('\033[1mENÉSIMOS PRIMEIROS TERMOS DE UMA PA (PROGRESSÃO ARITMÉTICA)\033[m')
 a1 = float(input('Primeiro termo: '))
 d = float(input('Razão: '))
 n = int(input('Número de termos a ser exibido desde o primeiro termo: '))
 print(f'Os {n} primeiros termos são: ')
-i = -1
-while i <= n-2:
-    i += 1
-    a = a1 + i * d
-    if i < n-1:
-        print(a, end=' -> ')
-    else:
-        print(a)
+n1 = 1
+while n1 != 0:
+    i = -1
+    while i <= n-2:
+        i += 1
+        a = a1 + i * d
+        if i < n-1:
+            print(a, end=' -> ')
+        else:
+            print(a)
+    n1 = int(input('Deseja mostrar mais quantos termos a partir do último termo exibido (0  para encerrar)? '))
+    n += n1
+print(f'Ao todo foram {n} termos exibidos. PA finalizada.')
 
