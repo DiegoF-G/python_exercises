72364bce071fd3acc4604b08ae78bbcafbd85227
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-05-02 09:44:03 -0300

    removing redundancies from python55.py

--- python55.py
+++ python55.py
@@ -1,14 +1,7 @@
 print('-'*5, '\033[1mANALISADOR DE GRUPO\033[m', '-'*5)
 n = int(input('Digite quantas pessoas tem no grupo: '))
-somaIdade = 0
-idadeMaiorM = 0
-idadeMaiorF = 0
-nomeMaisVelho = ''
-nomeMaisVelha = ''
-sexoMaisVelho = ''
-sexoMaisVelha = ''
-menorM = 0
-menorF = 0
+somaIdade = idadeMaiorM = idadeMaiorF = menorM = menorF = 0
+nomeMaisVelho = nomeMaisVelha = ''
 if n > 0:
     for j in range(1, n+1):
         print('-'*3, f'{j}ª Pessoa', '-'*3)
@@ -19,27 +12,22 @@ if n > 0:
         if j == 1 and sexo == 'M':
             idadeMaiorM = idade
             nomeMaisVelho = nome
-            sexoMaisVelho = sexo
         if j == 1 and sexo == 'F':
             idadeMaiorF = idade
             nomeMaisVelha = nome
-            sexoMaisVelha = sexo
         elif idade > idadeMaiorM and sexo == 'M':
             idadeMaiorM = idade
             nomeMaisVelho = nome
-            sexoMaisVelho = sexo
         elif idade > idadeMaiorF and sexo == 'F':
             idadeMaiorF = idade
             nomeMaisVelha = nome
-            sexoMaisVelha = sexo
         elif idade < 21 and sexo == 'M':
             menorM += 1
         elif idade < 21 and sexo == 'F':
             menorF += 1
     print(f'A idade média do grupo informado é de {somaIdade/n:.0f} anos.')
-    if sexoMaisVelho == 'M' and sexoMaisVelha == 'F':
-        print(f'{nomeMaisVelho} é o mais velho do grupo, com {idadeMaiorM} anos')
-        print(f'{nomeMaisVelha} é a mais velha do grupo, com {idadeMaiorF} anos')
+    print(f'{nomeMaisVelho} é o mais velho do grupo, com {idadeMaiorM} anos')
+    print(f'{nomeMaisVelha} é a mais velha do grupo, com {idadeMaiorF} anos')
     if menorM > 0:
         print(f'Ao todo são {menorM} homem(ns) com menos de 21 anos.')
     if menorF > 0:
