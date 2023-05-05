cb95afa529c3d8b44b5c775c5b89bf3e59c24093
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-05-05 14:01:42 -0300

    commit the exercise new exercise python70.py (PY  BANK)

---
+++ python70.py
@@ -0,0 +1,50 @@
+print('='*15, '\033[1:33mPY BANK\033[m', '='*15)
+x = int(input(' Valor do saque: R$ '))
+while x == 0:
+    r = input('Deseja sair do saque (S/N)? ').strip().upper()[0]
+    if r == 'S':
+        break
+    x = int(input(' Digite novamente o valor do saque: R$ '))
+i = j = k = m = n = 0
+while True:
+    if x >= 50:
+        i = x // 50
+        x -= i*50
+        if x == 0:
+            break
+    if 20 <= x < 50:
+        j = x // 20
+        x -= j*20
+        if x == 0:
+            break
+    if 10 <= x < 20:
+        k = x // 10
+        x -= k*10
+        if x == 0:
+            break
+    if 5 <= x < 10:
+        m = x // 5
+        x -= m*5
+        if x == 0:
+            break
+    if x < 5:
+        n = x
+        if x == n:
+            break
+print('-'*37)
+if i > 0:
+    print(f'Total de {i} cédulas de R$50,00')
+if j > 0:
+    print(f'Total de {j} cédulas de R$20,00')
+if k > 0:
+    print(f'Total de {k} cédulas de R$10,00')
+if m > 0:
+    print(f'Total de {m} cédulas de R$5,00')
+if n > 0:
+    print(f'Total de {n} cédulas de R$1,00')
+print('\033[0:36mObrigado, volte sempre!\033[m')
+
+# R$50, R$20, R$10 e R$1 são as notas nesse caixa eletrônico
+
+
+
