8fc7464a50322903136fe7a99f3d7b34de2c194d
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-05-07 14:35:02 -0300

    improving python71.py

--- python71.py
+++ python71.py
@@ -1,10 +1,15 @@
 nExtenso012 = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze')
 nExtenso1320 = ('Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
 nExtenso020 = nExtenso012 + nExtenso1320
-n = int(input('Digite um inteiro de 0 a 20: '))
-while not 0 <= n <= 20:
-    n = int(input('Número inválido. Digite novamente um número inteiro de 0 a 20: '))
-print(f'Você digitou o número: \033[1m{nExtenso020[n]}\033[m')
+while True:
+    n = int(input('Digite um inteiro de 0 a 20: '))
+    while not 0 <= n <= 20:
+        n = int(input('Número inválido. Digite novamente um número inteiro de 0 a 20: '))
+    print(f'Você digitou o número: \033[1m{nExtenso020[n]}\033[m')
+    c = input('Deseja continuar (S/N)? ').strip()[0]
+    if c in 'nN':
+        break
+
 
 
 
