223ce40246a9f6670566959c588b6933801db049
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-05-08 02:43:28 -0300

    commit the new exercise python73.py

---
+++ python73.py
@@ -0,0 +1,37 @@
+from random import randint
+while True:
+    rT = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+    for i, t in enumerate(rT):
+        if i == 0:
+            print(f'Os números inteiros positivos sorteados foram: {t}', end=', ')
+        elif i < len(rT) - 1:
+            print(f'{t}', end=', ')
+        else:
+            print(f'{t}')
+    print(f'O menor e o maior valor sorteados, respectivamente, foram {min(rT)} e {max(rT)}')
+    c = input('Deseja sortear novos números (S/N)? ')
+    if c in 'nN':
+        break
+
+# from random import randint
+# rT = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+# menor = maior = rT[0]
+# for i, t in enumerate(rT):
+#     if i == 0:
+#         print(f'Os números inteiros positivos sorteados foram: {t}', end=', ')
+#     elif i < len(rT) - 1:
+#        print(f'{t}', end=', ')
+#     else:
+#        print(f'{t}')
+#     if rT[i] > maior:
+#         maior = rT[i]
+#     elif rT[i] < menor:
+#         menor = rT[i]
+# print(f'O menor e o maior valor sorteados, respectivamente, foram {menor} e {maior}')
+
+
+
+
+
+
+
