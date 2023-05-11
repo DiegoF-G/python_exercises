688e32e283535c814b533abb9852667d9f51bd8c
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-05-11 12:23:31 -0300

    commit the new exercise python78.py

---
+++ python78.py
@@ -0,0 +1,19 @@
+num = []
+while True:
+    n = float(input('Digite um número: '))
+    if n in num:
+        print('Número já digitado anteriormente...não será salvo novamente.')
+    else:
+        num.append(n)
+    c = input('Deseja continuar digitando mais números (S/N)? ').strip()[0]
+    while c not in 'sSnN':
+        c = input('Deseja continuar digitando mais números (S/N)? ').strip()[0]
+    if c in 'nN':
+        break
+num.sort()
+print(f'A lista formada pelos valores digitados, em ordem crescente, foi: {num}')
+
+
+
+
+
