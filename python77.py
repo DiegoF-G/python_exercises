ad13bf7dd6299385f03a387a253efab7441a1518
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-05-11 02:32:12 -0300

    simplifying python77.py

--- python77.py
+++ python77.py
@@ -1,18 +1,17 @@
 k = int(input('Digite quantos números deseja digitar: '))
 num = []
-for i in range(1, k+1):
+for i in range(0, k):
     num.append(float(input(f'Digite um número: ')))
 print(f'A lista dos {k} números digitados é: {num}')
+print(f'O maior deles é {max(num)} na(s) posição(ões)', end=' ')
 for i, n in enumerate(num):
-    if i == num.index(max(num)):
-        print(f'O maior deles é {n} na(s) posição(ões) {i}', end=' ')
-    elif n == max(num) and i > num.index(max(num)):
+    if n == max(num):
         print(i, end=' ')
+print(f'\nO menor deles é {min(num)} na(s) posição(ões)', end=' ')
 for i, n in enumerate(num):
-    if i == num.index(min(num)):
-        print(f'\nO menor deles é {n} na(s) posição(ões) {i}', end=' ')
-    elif n == min(num) and i > num.index(min(num)):
+    if n == min(num):
         print(i, end=' ')
+
 
 
 
