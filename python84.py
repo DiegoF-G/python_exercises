347b482a1db50c4c4c737c00face8d0875a24b82
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-05-19 00:10:53 -0300

    commit the new exercise python84.py

---
+++ python84.py
@@ -0,0 +1,12 @@
+from bisect import insort
+num = [[], []]
+for i in range(0, 10):
+    n = int(input(f'Digite o {i+1}ª número inteiro: '))
+    if n % 2 == 0:
+        insort(num[0], n)
+    else:
+        insort(num[1], n)
+t = (print(f'\nOs números pares digitados foram: {num[0]}'), print(f'Os números ímpares digitados foram: {num[1]}'))
+
+
+
