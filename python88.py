6aa4015a6487d2a90454a6404feed26337b96c51
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-05-20 21:27:48 -0300

    commit the new exercise python88.py (BOLETIM)

---
+++ python88.py
@@ -0,0 +1,27 @@
+print('\033[1mBOLETIM\033[m')
+nomesNotas = []
+while True:
+    nomeNota = [None, []]
+    nomeNota[0] = input('Nome: ')
+    nomeNota[1].append(float(input('Nota N1: ')))
+    nomeNota[1].append(float(input('Nota N2: ')))
+    nomesNotas.append(nomeNota)
+    c = input('Deseja continuar digitando mais nomes e notas (S/N)? ').strip()[0]
+    while c not in 'sSnN':
+        c = input('Deseja continuar digitando mais nomes e notas (S/N)? ').strip()[0]
+    if c in 'nN':
+        break
+t = (print('-='*35), print(f'No.{"Nome":^15}{"Média":^15}'))
+for i in range(0, len(nomesNotas)):
+    print(f'{i+1} {nomesNotas[i][0]:^15} {(nomesNotas[i][1][0] + nomesNotas[i][1][1]) / 2:^15}')
+while True:
+    a = int(input('\nMostrar notas de qual pessoa (digite o número correspondente dela, 0 para encerra)? '))
+    if a != 0:
+        print(f'\n{nomesNotas[a-1]}')
+    else:
+        break
+print('\n\033[1mPrograma encerrado.\033[m')
+
+
+
+
