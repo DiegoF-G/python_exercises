0a5dc452abda5bf12a3e08021430e3b57161b5f8
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-05-20 21:51:09 -0300

    commit the new exercise python88.py again with changes (BOLETIM)

--- python88.py
+++ python88.py
@@ -11,14 +11,16 @@ while True:
         c = input('Deseja continuar digitando mais nomes e notas (S/N)? ').strip()[0]
     if c in 'nN':
         break
-t = (print('-='*45), print(f'No.{"Nome":^15}{"Média":^15}'))
+t = (print('-='*40), print(f'No.{"Nome":^15}{"Média":^15}'))
 for i in range(0, len(nomesNotas)):
     print(f'{i+1} {nomesNotas[i][0]:^15} {(nomesNotas[i][1][0] + nomesNotas[i][1][1]) / 2:^15}')
 while True:
     a = int(input('\nMostrar notas de qual pessoa (digite o número correspondente dela, 0 para encerra)? '))
-    if a != 0:
+    if a < len(nomesNotas):
         print(f'\n{nomesNotas[a-1]}')
-    else:
+    elif a > len(nomesNotas) or a < 0:
+        a = int(input('\nMostrar notas de qual pessoa (digite o número correspondente dela, 0 para encerra)? '))
+    elif a == 0:
         break
 print('\n\033[1mPrograma encerrado.\033[m')
 
