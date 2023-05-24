2af280c60ee20006cc4440d08cbb59ad2c42f94e
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-05-24 02:16:11 -0300

    commit another solution as a comment in python90.py

--- python90.py
+++ python90.py
@@ -19,6 +19,29 @@ for k, v in ranking:
     print(f'{r}ªLugar {k}: {v}')
     r += 1
     sleep(1)
+# Usando a funcionalidade itemgetter, do modulo padrao operator, no parametro key do sorted em vez da função lambda:
+# from random import randint
+# from time import sleep
+# from operator import itemgetter
+# print('123456', '\033[1mD6 GAME\033[m', '123456')
+# sleep(1)
+# resultados = dict()
+# for i in range(0, 4):
+#     resultados[f'Jogador {i+1}'] = randint(1, 6)
+#     print(f'Jogador {i+1} tirou {resultados[f"Jogador {i+1}"]} no D6')
+#     sleep(1.5)
+# print(f'\nResultado: {resultados}')
+# sleep(1)
+# print('\n\033[1mRANKING\033[m')
+# sleep(1)
+# ranking = sorted(resultados.items(), key=itemgetter(1), reverse=True)
+# r = 1
+# for k, v in ranking:
+#     if v == ranking[r-2][1] and r > 1:
+#         r -= 1
+#     print(f'{r}ªLugar {k}: {v}')
+#     r += 1
+#     sleep(1)
 
 
 
