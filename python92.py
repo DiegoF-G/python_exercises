6b73b695631f2283dfdc3c5469c677b18912436d
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-05-25 03:37:00 -0300

    Improving the exercise python92.py

--- python92.py
+++ python92.py
@@ -3,15 +3,15 @@ jogador['nome'] = input('Nome do jogador: ')
 n = int(input(f'Quantos jogos {jogador["nome"]} jogou? '))
 s = 0
 for i in range(0, n):
-    jogador[f'partida {i+1}'] = int(input(f'Quantos gol(s) {jogador["nome"]} fez na partida {i+1}: '))
+    jogador[f'partida {i+1}'] = int(input(f'Quantos gol(s) ele fez na partida {i+1}: '))
     s += jogador[f'partida {i+1}']
-jogador['total de gol(s)'] = s
+jogador['total'] = s
 print(f'\n\033[1m{"DESEMPENHO":^35}\033[m\n', '='*35)
 for k, v in jogador.items():
     if k == 'nome':
         print(f'O jogador {v} jogou {n} partida(s).')
-    elif k != 'nome' != 'total de gol(s)' != k:
+    elif k != 'nome' != 'total' != k:
         print(f'=> Na {k}, fez {v} gol(s).')
     else:
-        print(f'Foi um total de {jogador["total de gol(s)"]} gol(s).')
+        print(f'Foi um {k} de {v} gol(s).')
 
