c4a27bed0296cd02904b0fb33ef9f9a53ffac505
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-05-26 00:16:58 -0300

    commit the new exercise python93.py

---
+++ python93.py
@@ -0,0 +1,52 @@
+cores = {'limpa': '\033[m', 'vermelho': '\033[0:31m', 'brancoForte': '\033[1m'}
+print(f'{cores["brancoForte"]}CADASTRAMENTO{cores["limpa"]}')
+grupo = list()
+s = 0
+while True:
+    pessoa = dict()
+    pessoa['nome'] = input('Nome: ')
+    pessoa['sexo'] = input('Sexo (M/F): ').strip()[0]
+    while pessoa['sexo'] not in 'mMfF':
+        print(f'{cores["vermelho"]}Dado inválido! Favor digite novamente.{cores["limpa"]}')
+        pessoa['sexo'] = input('Sexo (M/F): ').strip()[0]
+    pessoa['idade'] = int(input('Idade: '))
+    s += pessoa['idade']
+    grupo.append(pessoa.copy())
+    c = input('Deseja continuar informando mais pessoas (S/N)? ').strip()[0]
+    while c not in 'sSnN':
+        print(f'{cores["vermelho"]}Resposta inválida! Favor digite novamente.{cores["limpa"]}')
+        c = input('Deseja continuar informando mais pessoas (S/N)? ').strip()[0]
+    if c in 'nN':
+        break
+m = s / len(grupo)
+mulheres = []
+acimaMedia = []
+for pessoa in grupo:
+    if pessoa['sexo'] in 'fF':
+        mulheres.append(pessoa['nome'])
+    if pessoa['idade'] > m:
+        acimaMedia.append(pessoa)
+print(f'{"-="*35}\nA) Foram {len(grupo)} pessoas cadastradas.\nB) A média de idade é de {m:.0f} anos.')
+if len(mulheres) > 0:
+    print('C) As mulheres cadastradas foram', end=' ')
+    for mulher in mulheres:
+        if mulher != mulheres[len(mulheres) - 1]:
+            print(f'{mulher},', end=' ')
+        else:
+            print(f'{mulher}.')
+else:
+    print('C) Nenhuma mulher foi cadastrada.')
+if len(acimaMedia) > 0:
+    print('D) Lista de pessoas com idade acima da média:')
+    for acima in acimaMedia:
+        print(f'{acima["nome"]:>20}; {acima["sexo"]}; {acima["idade"]}')
+else:
+    print(f'D) Todos cadastrados possuem a mesma idade, de {m} anos.')
+
+
+
+
+
+
+
+
