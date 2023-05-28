b0d6b81c99fa5d9f9cdfec5faec3832ddb6ce992
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-05-27 23:45:01 -0300

    fixing the exercise python95.py

--- python95.py
+++ python95.py
@@ -1,9 +1,11 @@
 def s_rect(a, b):
-    print(a * b)
+    return a*b
 
 
 base = float(input('Insira a medida da base do retângulo: '))
 h = float(input('Insira a medida da altura do retângulo (na mesma unidade de medida da base): '))
-print(f'O retângulo informado possui {s_rect(base, h)}u² de área')
+print(f'O retângulo informado possui {s_rect(base, h)} u² de área')
+
+
 
 
