05a5a85462aaa8049584f6a9d76e0195b24aa003
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-05-28 02:05:40 -0300

    improving python96.py

--- python96.py
+++ python96.py
@@ -1,10 +1,11 @@
-def printar_em_destaque(m):
+def escrever_em_destaque(m):
     msg = ' '.join(m.strip().split())
     print('-'*(len(msg) + 4))
     print(f'  \033[1m{msg}\033[m')
     print('-'*(len(msg) + 4))
 
 
-mensagem = input('Escreva uma mensagem qualquer para ser printada em destaque: ')
-printar_em_destaque(mensagem)
+mensagem = input('Escreva uma mensagem qualquer para ser exposta em destaque: ')
+escrever_em_destaque(mensagem)
+escrever_em_destaque('      CURSO EM VÍDEO         PYTHON MUNDO 3')
 
