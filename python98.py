c5e72cd87636b3f3d559225d9f97271fdd28dd0f
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-06-01 20:03:18 -0300

    commit the new exercise python98.py

---
+++ python98.py
@@ -0,0 +1,20 @@
+def informar_maior(*x):
+    if len(x) == 0:
+        print('Nenhum número informado, não existe maior.')
+    elif len(x) == 1:
+        print(f'Apenas 1 número informado, {x[0]}, o maior é ele mesmo.')
+    else:
+        print(f'Dos {len(x)} números informados o maior é {max(x)}.'),
+        print(f'Números informados: {str(x).replace("(", "").replace(")", "")}')
+
+
+testes = (informar_maior(1, 2, 3, 5.5, 0.2), print(), informar_maior(0), print(),
+          informar_maior(2, 22, 222, 222.001, 222.0005, 0), print(), informar_maior())
+
+
+
+
+
+
+
+
