641c37d7200dfb24e7e147283b3ef324ce07d418
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-06-01 23:22:44 -0300

    commit the new exercise python100.py

--- python100.py
+++ python100.py
@@ -1,9 +1,9 @@
 def voto(b):
-    '''
+    """
     A função voto() retorna sobre a obrigação de voto de um cidadão de acordo com as leis eleitorais em 2023 no Brasil.
     :param b: Ano de nascimento do cidadão
     :return: As strings: 'Voto NEGADO.', 'Voto OPCIONAL.' ou 'Voto OBRIGATÓRIO.'
-    '''
+    """
     from datetime import date
     y = date.today().year - b
     if 0 <= y < 16:
@@ -18,6 +18,7 @@ def voto(b):
 
 anoNascimento = int(input('Digite o ano de nascimento: '))
 print(voto(anoNascimento))
+
 
 
 
