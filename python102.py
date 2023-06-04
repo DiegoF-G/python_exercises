58a1b18f31da4e68eff8848af26ffda38ff76cf9
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-06-03 21:43:15 -0300

    commit the new exercise python102.py

---
+++ python102.py
@@ -0,0 +1,18 @@
+def ficha(n='<desconhecido>', g=0):
+    if type(g) != int:
+        g = 0
+    if type(n) != str:
+        n = '<desconhecido>'
+    n = ' '.join(n.strip().title().split())
+    print(f'O(A) jogador(a) {n} fez {g} gol(s) no campeonato.')
+    # or return instead print()
+
+
+testes = (ficha('  DieGo    Garcia ', 10), print(), ficha(11, 2), print(), ficha('AnA', 'Tres'), print(),
+          ficha())
+
+
+
+
+
+
