87ea8b8d823289e4ef333c01458dc7f3952b007c
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-06-11 20:14:16 -0300

    commit the new exercise python111.py and add news functions, leia_int (improved from python103.py) and leia_float, in both packs "meuPacotePy" and "dado"

--- meuPacotePy/__init__.py
+++ meuPacotePy/__init__.py
@@ -86,3 +86,36 @@ def leia_dinheiro(d):
     return float(x.replace(',', '.'))
 
 
+def leia_int(m='Digite um inteiro: '):
+    """
+    ->Função que lê um inteiro se, e somente se, a string digitada pode ser convertida em apenas no tipo primitivo int.
+      No caso de não ser possível essa conversão, é printado uma string informando que o valor é inválido.
+    :param m: string, que se espera ser uma mensagem solicitando qual o inteiro em questão
+    :return: inteiro, convertido da string se possível a conversão
+    """
+    while True:
+        try:
+            n = int(input(m))
+        except Exception as erro:
+            print(f'\033[0:31m{erro.__class__}. Ops! O valor digitado não é um inteiro. Favor digite novamente.\033[m')
+        else:
+            return n
+
+
+def leia_float(m='Digite um número real: '):
+    """
+    ->Função que lê um número real se, e somente se, a string digitada pode ser convertida no tipo primitivo float.
+      No caso de não ser possível essa conversão, é printado uma string informando que o valor é inválido.
+      Observação: Aceita o uso de vírgula para escrever o ponto flutuante.
+    :param m: string, que se espera ser uma mensagem solicitando qual o número real em questão
+    :return: float convertido da string, se possível a conversão
+    """
+    while True:
+        try:
+            x = float(input(m).replace(',', '.'))
+        except Exception as erro:
+            print(f'\033[0:31m{erro.__class__}. Ops! O valor digitado não é um número real. Favor digite novamente.\033[m')
+        else:
+            return x
+
+
--- python111.py
+++ python111.py
@@ -1,19 +1,3 @@
-# pack referente ao exercício 110, e tambem será usado em outros
-
-
-def leia_dinheiro(d):
-    """
-    ->Função que valida ("por cima", é uma validação incompleta) e amarzena a entrada de valores monetários.
-    :param d: string, que representa o float a ser convertido
-    :return: float, que representa a quantia monetária inserida
-    """
-    x = input(d)
-    while x.isalpha() or x.isspace() or x == '':
-        print('\033[0:31mPreço inválido, digite novamente.\033[m')
-        x = input(d)
-    return float(x.replace(',', '.'))
-
-
 def leia_int(m='Digite um inteiro: '):
     """
     ->Função que lê um inteiro se, e somente se, a string digitada pode ser convertida em apenas no tipo primitivo int.
@@ -45,4 +29,8 @@ def leia_float(m='Digite um número real: '):
             print(f'\033[0:31m{erro.__class__}. Ops! O valor digitado não é um número real. Favor digite novamente.\033[m')
         else:
             return x
+
+
+testes = (print(leia_int()), print(leia_float()))
+
 
