f502c791aa8d762bd031a575a2741d2467703652
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-06-11 23:32:46 -0300

    Update python112.py

--- python112.py
+++ python112.py
@@ -2,7 +2,7 @@ import urllib.request
 try:
     site = urllib.request.urlopen('http://www.pudim.com.br/')
 except Exception as erro:
-    print(f'{erro}O site Pudim não está acessível no momento.')
+    print(f'{erro} O site Pudim não está acessível no momento.')
 else:
     print('Consegui acessar o site Pudim com sucesso!')
     print(f'\nO respectivo HTML dele é:\n{site.read()}')
