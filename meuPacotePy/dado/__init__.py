86577368ce699fc1ab0eee6274621adb4e91df6c
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-06-11 22:44:22 -0300

    Improving functions leia_int and leia_float

--- meuPacotePy/dado/__init__.py
+++ meuPacotePy/dado/__init__.py
@@ -18,6 +18,7 @@ def leia_int(m='Digite um inteiro: '):
     """
     ->Função que lê um inteiro se, e somente se, a string digitada pode ser convertida no tipo primitivo inteiro.
       No caso de não ser possível essa conversão, é printado uma string informando que o valor é inválido.
+      Se a execução for interrompida, retorna o inteiro 0.
     :param m: string, que se espera ser uma mensagem solicitando qual o inteiro em questão
     :return: inteiro, convertido da string se possível a conversão
     """
@@ -25,7 +26,10 @@ def leia_int(m='Digite um inteiro: '):
         try:
             n = int(input(m))
         except Exception as erro:
-            print(f'\033[0:31m{erro.__class__}. Ops! O valor digitado não é um inteiro. Favor digite novamente.\033[m')
+            print(f'\033[0:31m{erro.__class__} Ops! O valor digitado não é um inteiro. Favor digite novamente.\033[m')
+        except KeyboardInterrupt as erro:
+            print(f'\n\033[0:31m{erro.__class__} O usuário interrompeu a execução.\033[m')
+            return 0
         else:
             return n
 
@@ -34,6 +38,7 @@ def leia_float(m='Digite um número real: '):
     """
     ->Função que lê um número real se, e somente se, a string digitada pode ser convertida no tipo primitivo float.
       No caso de não ser possível essa conversão, é printado uma string informando que o valor é inválido.
+      Se a execução for interrompida, retorna o inteiro 0.
       Observação: Aceita o uso de vírgula para escrever o ponto flutuante.
     :param m: string, que se espera ser uma mensagem solicitando qual o número real em questão
     :return: float convertido da string, se possível a conversão
@@ -42,7 +47,9 @@ def leia_float(m='Digite um número real: '):
         try:
             x = float(input(m).replace(',', '.'))
         except Exception as erro:
-            print(f'\033[0:31m{erro.__class__}. Ops! O valor digitado não é um número real. Favor digite novamente.\033[m')
+            print(f'\033[0:31m{erro.__class__} Ops! O valor digitado não é um número real. Favor digite novamente.\033[m')
+        except KeyboardInterrupt as erro:
+            print(f'\n\033[0:31m{erro.__class__} O usuário interrompeu a execução.\033[m')
+            return 0
         else:
             return x
-
