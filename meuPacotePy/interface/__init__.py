a9f77f15ad42e9e4981b03d04b9b511148c6a631
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-06-13 00:22:26 -0300

    fixing a problem in pack "interface" in pack "meuPacotePy", adding function "leia_int" in this pack for it

--- meuPacotePy/interface/__init__.py
+++ meuPacotePy/interface/__init__.py
@@ -1,3 +1,23 @@
+def leia_int(m='Digite um inteiro: '):
+    """
+    ->Função que lê um inteiro se, e somente se, a string digitada pode ser convertida no tipo primitivo inteiro.
+      No caso de não ser possível essa conversão, é printado uma string informando que o valor é inválido.
+      Se a execução for interrompida, retorna o inteiro 0.
+    :param m: string, que se espera ser uma mensagem solicitando qual o inteiro em questão
+    :return: inteiro, convertido da string se possível a conversão
+    """
+    while True:
+        try:
+            n = int(input(m))
+        except Exception as erro:
+            print(f'\033[0:31m{erro.__class__} Ops! O valor digitado não é um inteiro. Favor digite novamente.\033[m')
+        except KeyboardInterrupt as erro:
+            print(f'\n\033[0:31m{erro.__class__} O usuário interrompeu a execução.\033[m')
+            return 0
+        else:
+            return n
+
+
 def linha(l=42, c='-'):
     """
     ->Função que fornece uma linha de tamanho dado pelo primeiro parâmetro, o tamanho padrão são de 42 caractéres "-".
