c5173bb81dd069a3b845b46792a9631fd3364efa
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-06-22 19:39:09 -0300

    commit several improvements in functions of pythonExtraMap.py

--- meuPacotePy/__init__.py
+++ meuPacotePy/__init__.py
@@ -244,19 +244,22 @@ def cadastrar(arq, nome='desconhecido', idade=0):
             a.close()
 
 
-def lista_triplo(lista: list) -> list:
+def lista_triplo(lista: list = None) -> list:
     """
     ->Função que recebe uma lista de números e retorna uma lista com os respectivos triplos desses números.
     :param lista: lista de floats ou inteiros
     :return: lista com os triplos dos números em cada índice da lista de entrada, respectivamente
     """
+    if lista is None:
+        lista = []
     return list(map(lambda x: 3*x, lista))
 
 
-teste1 = (print('Teste 1'), print(lista_triplo([1, 2, 3, 4, 0.1, 0.005, 10.3333])), print())
+teste1 = (print('Teste 1'), print(lista_triplo([1, 2, 3, 4, 0.1, 0.005, 10.3333])), print(lista_triplo()), print())
 
 
-def lista_fahrenheit(lista_c: list) -> list:
+# (2)Função que recebe uma lista de temperaturas em graus Celsius e transforma numa lista de temperatura em Fahrenheit
+def lista_fahrenheit(lista_c: list = None) -> list:
     """
     ->Função que recebe uma lista de medidas de temperaturas em Celsius e transforma numa lista das mesmas em Fahrenheit.
     :param lista_c: lista de floats ou inteiros, sendo a lista de medidas de temperaturas em Celsius
@@ -265,22 +268,25 @@ def lista_fahrenheit(lista_c: list) -> list:
     return list(map(lambda tc: (tc * 9 / 5) + 32, lista_c))
 
 
-teste2 = (print('Teste 2'), print(lista_fahrenheit([0, 25, 37, 78, 100])), print())
+teste2 = (print('Teste 2'), print(lista_fahrenheit([0, 25, 37, 78, 100])), print(lista_fahrenheit()), print())
 
 
-def lista_quadrado(lista: list) -> list:
+def lista_quadrado(lista: list = None) -> list:
     """
     ->Função que recebe uma lista de números e retorna uma lista com os respectivos quadrados desses números.
     :param lista: lista de floats ou inteiros
     :return: lista com os quadrados dos números em cada índice da lista de entrada, respectivamente
     """
+    if lista is None:
+        lista = [0]
     return list(map(lambda x: x**2, lista))
 
 
-teste3 = (print('Teste 3'), print(lista_quadrado([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1.5, 0.05])), print())
+teste3 = (print('Teste 3'), print(lista_quadrado([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1.5, 0.05])), print(lista_quadrado()),
+          print())
 
 
-def palavras_com_mais_de_5_letras_em_(frase: str) -> list:
+def palavra_com_mais_de_5_letras_em_(frase: str = '') -> list:
     """
     ->Função que recebe uma frase e retorna uma lista com as palavras que possuem mais de 5 letras.
     :param frase: string, sendo uma frase qualquer
@@ -292,4 +298,5 @@ def palavras_com_mais_de_5_letras_em_(frase: str) -> list:
     return list(filter(lambda palavra: len(palavra) >= 5, frase_formatada))
 
 
-teste4 = (print('Teste 4'), print(palavras_com_mais_de_5_letras_em_('Aprender Python aqui na Alura é muito bom')))
+teste4 = (print('Teste 4'), print(palavra_com_mais_de_5_letras_em_('Aprender Python aqui na Alura é muito bom')),
+          print(palavra_com_mais_de_5_letras_em_('TESTE!!')))
--- pythonExtraMap.py
+++ pythonExtraMap.py
@@ -2,20 +2,22 @@
 # função que pode ser anônima (função lambda) ou outra definida breviamente.
 
 # (1)Função que recebe uma lista de números e retorna uma lista do triplo dos respectivos números
-def lista_triplo(lista: list) -> list:
+def lista_triplo(lista: list = None) -> list:
     """
     ->Função que recebe uma lista de números e retorna uma lista com os respectivos triplos desses números.
     :param lista: lista de floats ou inteiros
     :return: lista com os triplos dos números em cada índice da lista de entrada, respectivamente
     """
+    if lista is None:
+        lista = []
     return list(map(lambda x: 3*x, lista))
 
 
-teste1 = (print('Teste 1'), print(lista_triplo([1, 2, 3, 4, 0.1, 0.005, 10.3333])), print())
+teste1 = (print('Teste 1'), print(lista_triplo([1, 2, 3, 4, 0.1, 0.005, 10.3333])), print(lista_triplo()), print())
 
 
 # (2)Função que recebe uma lista de temperaturas em graus Celsius e transforma numa lista de temperatura em Fahrenheit
-def lista_fahrenheit(lista_c: list) -> list:
+def lista_fahrenheit(lista_c: list = None) -> list:
     """
     ->Função que recebe uma lista de medidas de temperaturas em Celsius e transforma numa lista das mesmas em Fahrenheit.
     :param lista_c: lista de floats ou inteiros, sendo a lista de medidas de temperaturas em Celsius
@@ -24,25 +26,28 @@ def lista_fahrenheit(lista_c: list) -> list:
     return list(map(lambda tc: (tc * 9 / 5) + 32, lista_c))
 
 
-teste2 = (print('Teste 2'), print(lista_fahrenheit([0, 25, 37, 78, 100])), print())
+teste2 = (print('Teste 2'), print(lista_fahrenheit([0, 25, 37, 78, 100])), print(lista_fahrenheit()), print())
 
 
 # (3)Função que recebe uma lista de números e retorna uma lista dos quadrados dos respectivos números
-def lista_quadrado(lista: list) -> list:
+def lista_quadrado(lista: list = None) -> list:
     """
     ->Função que recebe uma lista de números e retorna uma lista com os respectivos quadrados desses números.
     :param lista: lista de floats ou inteiros
     :return: lista com os quadrados dos números em cada índice da lista de entrada, respectivamente
     """
+    if lista is None:
+        lista = [0]
     return list(map(lambda x: x**2, lista))
 
 
-teste3 = (print('Teste 3'), print(lista_quadrado([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1.5, 0.05])), print())
+teste3 = (print('Teste 3'), print(lista_quadrado([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1.5, 0.05])), print(lista_quadrado()),
+          print())
 
 
-# (4)Um trecho de código que recebe uma frase digitada pela pessoa usuária e filtre apenas as palavras com tamanho
+# (4)Uma função que recebe uma frase digitada pela pessoa usuária e filtre apenas as palavras com tamanho
 # maior ou igual a 5, exibindo-as numa lista. Aqui usarei a função built-in Filter.
-def palavras_com_mais_de_5_letras_em_(frase: str) -> list:
+def palavra_com_mais_de_5_letras_em_(frase: str = '') -> list:
     """
     ->Função que recebe uma frase e retorna uma lista com as palavras que possuem mais de 5 letras.
     :param frase: string, sendo uma frase qualquer
@@ -54,4 +59,5 @@ def palavras_com_mais_de_5_letras_em_(frase: str) -> list:
     return list(filter(lambda palavra: len(palavra) >= 5, frase_formatada))
 
 
-teste4 = (print('Teste 4'), print(palavras_com_mais_de_5_letras_em_('Aprender Python aqui na Alura é muito bom')))
+teste4 = (print('Teste 4'), print(palavra_com_mais_de_5_letras_em_('Aprender Python aqui na Alura é muito bom')),
+          print(palavra_com_mais_de_5_letras_em_('TESTE!!')))
