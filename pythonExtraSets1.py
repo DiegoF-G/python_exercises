c636202433e8c5d06f298c50ac7497a694903c52
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-07-21 11:45:49 -0300

    Updating

--- pythonExtraSets1.py
+++ pythonExtraSets1.py
@@ -9,17 +9,17 @@ setCarros = set(listaCarros)
 cabecalho('Temos a seguinte lista e set, respectivamente:', 50)
 listaSet = (print(listaCarros), print(setCarros))
 
-cabecalho('Sets não possuem elementos repetidos e caso tenha eles são "unificados"', 50)
+cabecalho('Sets não possuem elementos repetidos e caso tenha eles são "unificados"', 75)
 print(f'Quantidade de elementos na lista de carros: {len(listaCarros)}')
 print(f'Quantidade de elementos no conjunto (set) de carros: {len(setCarros)}')
 
-cabecalho('Sets podem possuir dados de diferentes tipos e são mutáveis', 70)
+cabecalho('Sets podem possuir dados de diferentes tipos e são mutáveis', 65)
 onix = {2017, "Onix", 37123.04, True}
 print(onix)
 onix.add('1.0 turbo')
 print(onix)
 
-cabecalho('Sets não são enumeráveis (não são indexados), diferente do que pode ser um conjunto na Matemática', 120)
+cabecalho('Sets não são enumeráveis (não são indexados), diferente do que pode ser um conjunto na Matemática', 105)
 try:
     print(setCarros[0])
 finally:
