8b4fe8a10a461ae535b057ad6284fd2fde10f24d
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-08-08 12:21:18 -0300

    Updating files

--- pythonExtraListDictComprehension.py
+++ pythonExtraListDictComprehension.py
@@ -1,6 +1,8 @@
-# Nesse arquivo vou explorar as estruturas do List e Dict Comprehension, sendo uma ferramenta do Py para criar listas
-# de maneira mais prática e concisa a partir de outras listas.
-# Também vou utilizar a função built-in zip para criar listas de tuplas, muito útil para iterações.
+"""Nesse arquivo vou explorar as estruturas do List e Dict Comprehension, sendo uma ferramenta do Py para criar listas
+   de maneira mais prática e concisa a partir de outras listas.
+   Também vou utilizar a função built-in zip para criar listas de tuplas, muito útil para iterações.
+"""
+
 
 # (1)Função que recebe uma lista de floats e devolve uma lista com os respectivos dobros
 def lista_dobro(lista: list = None) -> list:
@@ -21,22 +23,22 @@ teste1 = (print('TESTE 1'), print(lista_dobro([10, 150, 1000, 10.5, 0.75])), pri
 # (2)Uma lista gerada por uma list comprehension que armazena somente o valor numérico de cada tupla caso o primeiro
 # elemento seja 'Apartamento':
 aluguel = [('Apartamento', 1700), ('Apartamento', 1400), ('Casa', 2150), ('Apartamento', 1900), ('Casa', 1100)]
-apAluguel = [aluguel[i][1] for i in range(len(aluguel)) if aluguel[i][0] == 'Apartamento']
-teste2 = (print('TESTE 2'), print(apAluguel), print())
+ap_aluguel = [aluguel[i][1] for i in range(len(aluguel)) if aluguel[i][0] == 'Apartamento']
+teste2 = (print('TESTE 2'), print(ap_aluguel), print())
 
 # (3)
 meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
 despesa = [860, 490, 1010, 780, 900, 630, 590, 770, 620, 560, 840, 360]
-despesasAno = {meses[i]: despesa[i] for i in range(12)}
-teste3 = (print('TESTE 3'), print(despesasAno), print())
+despesas_ano = {meses[i]: despesa[i] for i in range(12)}
+teste3 = (print('TESTE 3'), print(despesas_ano), print())
 
 # (4)Foi repassado uma lista de 20 estudantes e suas respectivas médias finais. Precisamos
 # selecionar estudantes que tenham média final maior ou igual a 9.0
-nomesEstudantes = ['Enrico Monteiro', 'Luna Pereira', 'Anthony Silveira', 'Letícia Fernandes',
+nomes_estudantes = ['Enrico Monteiro', 'Luna Pereira', 'Anthony Silveira', 'Letícia Fernandes',
                    'João Vitor Nascimento', 'Maysa Caldeira', 'Diana Carvalho', 'Mariane da Rosa',
                    'Camila Fernandes', 'Levi Alves', 'Nicolas da Rocha', 'Amanda Novaes',
                    'Laís Moraes', 'Letícia Oliveira', 'Lucca Novaes', 'Lara Cunha',
                    'Beatriz Martins', 'João Vitor Azevedo', 'Stephany Rosa', 'Gustavo Henrique Lima']
-mediasEstudantes = [5.4, 4.1, 9.1, 5.3, 6.9, 3.1, 10.0, 5.0, 8.2, 5.5, 8.1, 7.4, 5.0, 3.7, 8.1, 6.2, 6.1, 5.6, 6.7, 8.2]
-bolsistas = {nome: media for nome, media in zip(nomesEstudantes, mediasEstudantes) if media >= 9.0}
+medias_estudantes = [5.4, 4.1, 9.1, 5.3, 6.9, 3.1, 10.0, 5.0, 8.2, 5.5, 8.1, 7.4, 5.0, 3.7, 8.1, 6.2, 6.1, 5.6, 6.7, 8.2]
+bolsistas = {nome: media for nome, media in zip(nomes_estudantes, medias_estudantes) if media >= 9.0}
 teste4 = (print('TESTE 4'), print(bolsistas))
