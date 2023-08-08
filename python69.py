bd82bb0aaa84ff6b28dddd635548b94f805be54d
Diego <108773248+DiegoF-G@users.noreply.github.com>
2023-08-08 11:33:16 -0300

    Updating files

--- python69.py
+++ python69.py
@@ -2,35 +2,36 @@ print('='*16)
 print(' \033[1mBARATÃO STORE\033[m')
 print('='*16)
 s = caro = 0
-produtoBarato = ''
-name = input('Nome do produto: ')
-price = float(input('Preço do produto: R$ '))
-barato = price
+produto_barato = 'produto barato'
+nome = input('Nome do produto: ')
+preco = float(input('Preço do produto: R$ '))
+barato = preco
 while True:
-    s += price
-    if price < barato:
-        barato = price
-        produtoBarato = name
-    if price > 1000:
+    s += preco
+    if preco <= barato:
+        barato = preco
+        produto_barato = nome
+    if preco > 1000:
         caro += 1
     c = input('Continuar comprando (S/N)? ').strip().upper()[0]
-    print(' ')
+    print()
     while c not in 'SN':
         c = input('Continuar comprando (S/N)? ').strip().upper()[0]
-        print(' ')
+        print()
     if c == 'N':
         break
-    name = input('Nome do produto: ')
-    price = float(input('Preço do produto: R$ '))
-print('-'*20)
+    nome = input('Nome do produto: ')
+    preco = float(input('Preço do produto: R$ '))
+print('-'*50)
 print(f'O valor total da compra foi de R${s}')
 print(f'Temos {caro} produto(s) custando mais de R$1000.00')
-print(f'O produto mais barato foi {produtoBarato} custando R${barato}')
+print(f'O produto mais barato foi {produto_barato} custando R${barato}')
+
 # print('='*16)
 # print(' \033[1mBARATÃO STORE\033[m')
 # print('='*16)
 # s = caro = barato = i = 0
-# produtoBarato = ''
+# produto_barato = ''
 # while True:
 #     i += 1
 #     name = input('Nome do produto: ')
@@ -43,7 +44,7 @@ print(f'O produto mais barato foi {produtoBarato} custando R${barato}')
 #     s += price
 #     if i == 1 or price < barato:
 #         barato = price
-#         produtoBarato = name
+#         produto_barato = name
 #     if price > 1000:
 #         caro += 1
 #     if c == 'N':
@@ -51,11 +52,4 @@ print(f'O produto mais barato foi {produtoBarato} custando R${barato}')
 # print('-'*20)
 # print(f'O valor total da compra foi de R${s}')
 # print(f'Temos {caro} produto(s) custando mais de R$1000.00')
-# print(f'O produto mais barato foi {produtoBarato} custando R${barato}')
-
-
-
-
-
-
-
+# print(f'O produto mais barato foi {produto_barato} custando R${barato}')
